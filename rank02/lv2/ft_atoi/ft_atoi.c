a8188c4c0058f4ee44cac0ad804ba57c893dee4a
Miyuki Ito <mito@c1r3p9.hive.fi>
2024-02-07 16:29:13 +0200

    modified atoi and last word

--- rank02/lv2/ft_atoi/ft_atoi.c
+++ rank02/lv2/ft_atoi/ft_atoi.c
@@ -42,6 +42,28 @@ int ft_atoi(const char *str)
     return (res * negative);
 }
 
+//other solution
+int	ft_atoi(const char *str)
+{
+	int i = 0;
+	int res = 0;
+	int neg = 1;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			neg = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res * neg);
+}
+
 #include <stdio.h>
 
 int main()
