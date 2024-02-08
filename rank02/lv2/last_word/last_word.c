fde44911959970737c871808aac8b99e8c101f45
Miyuki Ito <mito@c1r3p7.hive.fi>
2024-02-08 11:28:30 +0200

    added own solution for lastword

--- rank02/lv2/last_word/last_word.c
+++ rank02/lv2/last_word/last_word.c
@@ -1,29 +1,56 @@
-// haven't practiced yet: only this solution works
-
 #include <unistd.h>
 
-void	last_word(char *str)
+void last_word(char *str)
 {
-	int	j = 0;
 	int i = 0;
+	int tracker = 0;
 
-	while (str[i])
+	while (str[i] != '\0')
 	{
-		if (str[i] == ' ' && str[i + 1] >= 33 && str[i + 1] <= 126)
-			j = i + 1;
+		if ((str[i] == ' ' || str[i] == '\t') && (str[i + 1] >= 33 && str[i + 1] <= 126))
+			tracker = i + 1;
 		i++;
 	}
-	while (str[j] >= 33 && str[j] <= 127)
+	while (str[tracker] >= 33 && str[tracker] <= 126)
 	{
-		write(1, &str[j], 1);
-		j++;
+		write(1, &str[tracker], 1);
+		tracker++;
 	}
 }
 
-int		main(int argc, char **argv)
+
+int main(int argc, char **argv)
 {
 	if (argc == 2)
 		last_word(argv[1]);
 	write(1, "\n", 1);
 	return (0);
 }
+
+
+
+// void	last_word(char *str)
+// {
+// 	int	j = 0;
+// 	int i = 0;
+
+// 	while (str[i])
+// 	{
+// 		if ((str[i] == ' ' || str[i] == '\t') && (str[i + 1] >= 33 && str[i + 1] <= 126))
+// 			j = i + 1;
+// 		i++;
+// 	}
+// 	while (str[j] >= 33 && str[j] <= 127)
+// 	{
+// 		write(1, &str[j], 1);
+// 		j++;
+// 	}
+// }
+
+// int		main(int argc, char **argv)
+// {
+// 	if (argc == 2)
+// 		last_word(argv[1]);
+// 	write(1, "\n", 1);
+// 	return (0);
+// }
