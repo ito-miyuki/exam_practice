ba8a4e400a1b0c84a50af5864914d23c0d7a3ac0
Miyuki Ito <mito@c1r2p5.hive.fi>
2024-02-21 15:05:53 +0200

    added rstr capitalizer

--- rank02/lv3/rstr_capitalizer/rstr_capitalizer.c
+++ rank02/lv3/rstr_capitalizer/rstr_capitalizer.c
@@ -86,3 +86,42 @@ int main(int argc, char **argv)
 // 	}
 // 	return (0);
 // }
+
+//other solutions
+#include <unistd.h>
+
+void rstr_capitalizer(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		if ((str[i + 1] == ' ' || str[i + 1] == '\t' || str[i + 1] == '\0'))
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+		}
+		else if ((str[i] >= 'A' && str[i] <= 'Z') && (str[i - 1] == ' ' || str[i - 1] != '\t' || str[i - 1] != '\0'))
+			str[i] += 32;
+		else if ((str[i] >= 'A' && str[i] <= 'Z'))
+			str[i] += 32;
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		int i = 1;
+		while (argv[i] != NULL)
+		{
+			rstr_capitalizer(argv[i]);
+			i++;
+		}
+	}
+	else
+		write(1, "\n", 1);
+	return (0);
+}
