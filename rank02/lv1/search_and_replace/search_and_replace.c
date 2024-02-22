f304d881c8b0a9e23fc0ccf9385c7deaeb5c5d2c
Miyuki Ito <mito@c1r2p5.hive.fi>
2024-02-22 09:45:22 +0200

    deleted s and r and modified tab mult

--- rank02/lv1/search_and_replace/search_and_replace.c
+++ rank02/lv1/search_and_replace/search_and_replace.c
@@ -31,3 +31,25 @@ int main(int argc, char **argv)
     write(1, "\n", 1);
     return (0);
 }
+
+//other solutions
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	if (argc == 4)
+	{
+		if (argv[2][1] == '\0' && argv[3][1] == '\0')
+		{
+			int i = 0;
+			while (argv[1][i] != '\0')
+			{
+				if (argv[1][i] == argv[2][0])
+					argv[1][i] = argv[3][0];
+				write(1, &argv[1][i], 1);
+				i++;
+			}
+		}
+	}
+	write(1, "\n", 1);
+}
--- rank02/lv3/tab_mult/tab_mult.c
+++ rank02/lv3/tab_mult/tab_mult.c
@@ -76,3 +76,62 @@ int main(int argc, char **argv)
 	return (0);
 }
 
+
+
+//other solutions
+void ft_putnbr(int num)
+{
+	char c;
+	if (num < 10)
+	{
+		c = num + 48;
+		write(1, &c, 1);
+	}
+	else
+	{
+		ft_putnbr(num / 10);
+		num = num % 10;
+		c = num + 48;
+		write(1, &c, 1);
+	}
+}
+
+int ft_atoi(char *num)
+{
+	int i = 0;
+	int res = 0;
+	while (num[i] != '\0')
+	{
+		res = res * 10 + (num[i] - '0');
+		i++;
+	}
+	return (res);
+}
+
+void ft_tab_mult(char *char_num)
+{
+	int num = ft_atoi(char_num);
+	int i = 1;
+	while (i <= 9)
+	{
+		ft_putnbr(i);
+		write(1, " ", 1);
+		write(1, "x", 1);
+		write(1, " ", 1);
+		ft_putnbr(num);
+		write(1, " ", 1);
+		write(1, "=", 1);
+		write(1, " ", 1);
+		ft_putnbr(i * num);
+		write(1, "\n", 1);
+		i++;
+	}
+}
+int main(int argc, char **argv)
+{
+	if (argc == 2)
+		ft_tab_mult(argv[1]);
+	else
+		write(1, "\n", 1);	
+	return (0);
+}
