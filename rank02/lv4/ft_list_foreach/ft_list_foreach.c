22826bee0cb2d75c08691e5e155e2c49e017f9a5
Miyuki Ito <miyuki.ito@slurp.coffee>
2024-02-25 21:58:24 +0200

    added ft_list_foreach

---
+++ rank02/lv4/ft_list_foreach/ft_list_foreach.c
@@ -0,0 +1,13 @@
+#include "ft_list.h"
+
+void    ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+    while (begin_list != NULL) //リストにまだnodeがあるあいだ
+    {
+        if (begin_list->data) //dataが有効な値を指していたら
+            (*f)(begin_list->data); //それに対して関数を実行
+        begin_list = begin_list->next; //次のnodeへ
+    }
+}
+
+
