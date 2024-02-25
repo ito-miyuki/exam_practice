888f81211192bca5a3677f8c6ccebf6c6eb3b001
Miyuki Ito <miyuki.ito@slurp.coffee>
2024-02-25 21:33:43 +0200

    added ft_list_remove_if

---
+++ rank02/lv4/ft_list_remove_if/ft_list_remove_if.c
@@ -0,0 +1,44 @@
+#include "ft_list.h"
+#include <stdlib.h>
+
+void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+{
+	if (begin_list == NULL || *begin_list == NULL) //ポインタ自体が無効　または　リストが空
+		return;
+
+	t_list *cur = *begin_list;
+
+	if (cmp(cur->data, data_ref) == 0) //現在のノードの「データ」が参照データと等しい場合
+	{
+		*begin_list = cur->next; //begin listがさしているものをcur(temp)を使って次のものにし、
+		free(cur); // //トップだったものを削除
+		ft_list_remove_if(begin_list, data_ref, cmp); // 更新されたリスト（現在の要素が削除された後のリスト）に対して同じ処理を繰り返す
+	}
+    else
+    {
+        // 現在のノードが重複していない場合、次のノードに対して処理を適用
+        ft_list_remove_if(&(*begin_list)->next, data_ref, cmp);
+    }
+}
+
+
+//　github からのsolution
+void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+{
+    if (begin_list == NULL || *begin_list == NULL)
+        return ;
+
+    t_list *cur = *begin_list;
+
+    if (cmp(cur->data, data_ref) == 0)
+    {
+        *begin_list = cur->next;
+        free(cur);
+        ft_list_remove_if(begin_list, data_ref, cmp);
+    }
+    cur = *begin_list;
+    ft_list_remove_if(&cur->next, data_ref, cmp);
+}
+
+//練習
+
--- rank02/lv4/sort_list/sort_list.c
+++ rank02/lv4/sort_list/sort_list.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-//#include "list.h"
+#include "list.h"
 
 typedef struct s_list t_list;
 
@@ -28,6 +28,33 @@ t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
 		if (((*cmp)(lst->data, lst->next->data)) == 0)
 		{
 			ft_swap(&lst->data, &lst->next->data);
+			lst = tmp;　//
+		}
+		else
+			lst = lst->next;
+	}
+	lst = tmp;
+	return (lst);
+}
+
+
+// other solutions
+#include "list.h"
+#include <stdlib.h>
+
+t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
+{
+	int	swap;
+	t_list	*tmp;
+
+	tmp = lst;
+	while(lst->next != NULL)
+	{
+		if (((*cmp)(lst->data, lst->next->data)) == 0)
+		{
+			swap = lst->data;
+			lst->data = lst->next->data;
+			lst->next->data = swap;
 			lst = tmp;
 		}
 		else
