4e0f16b41b13e615370dc62af559d71639a8c7c6
Miyuki Ito <miyuki.ito@slurp.coffee>
2024-02-26 21:15:12 +0200

    added expand str

--- rank02/lv3/expand_str/expand_str.c
+++ rank02/lv3/expand_str/expand_str.c
@@ -1,5 +1,33 @@
 #include <unistd.h>
 
+void ft_expand_str(char *str)
+{
+    int i = 0;
+    int word_started = 0;
+    while (str[i] != '\0')
+    {
+        while (str[i] == ' ' || str[i] == '\t')
+            i++;
+        if (str[i] != '\0' && word_started == 1)
+            write(1, "   ", 3);
+        while (str[i] != ' ' && str[i] != '\t' && str[i] != '\0')
+        {
+            write(1, &str[i], 1);
+            i++;
+            word_started = 1;
+        }
+    }
+}
+
+int main (int argc, char **argv)
+{
+    if (argc == 2)
+        ft_expand_str(argv[1]);
+    write(1, "\n", 1);
+    return (0);
+}
+
+//other solution
 int main (int argc, char **argv)
 {
     if (argc == 2)
