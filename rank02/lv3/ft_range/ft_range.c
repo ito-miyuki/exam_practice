70740d4b78012c153ef7cccc171514c24a08d64a
Miyuki Ito <mito@c1r3p9.hive.fi>
2024-03-27 17:38:31 +0200

    added moulinette pass solution

--- rank02/lv3/ft_range/ft_range.c
+++ rank02/lv3/ft_range/ft_range.c
@@ -1,49 +1,69 @@
 #include <stdlib.h>
-#include <stdio.h>
 
 int     *ft_range(int start, int end)
 {
-    int *res;
-    int range;
-    int i = 0;
-
-    if (start <= end) //プラスに向かって行くケース
-        range = (end - start) + 1;
-    else if (end < start) //マイナスに向かって行くケース
-        range = (start - end) + 1;
-    res = malloc(sizeof(int) * range);
-    if (!res)
-        return (0);
-    if (start == end)
-    {
-        res[i] = start;
-        printf("%d", res[i]);
-    }
-    else if (start < end)//プラスに向かって行くケース
-    {
-        while (i < range)
-        {
-            res[i] = start;
-            printf("%d", res[i]); // delete it 
-            i++;
-            start++;
-        }
-    }
-    else if (end < start) //マイナスに向かって行くケース
-    {
-        while (i < range)
-        {
-            res[i] = start;
-            printf("%d", res[i]); // delete it 
-            i++;;
-            start--;
-        }
-    }
-    return (res);
+	int *res;
+	int size;
+	int i = 0;
+	if (start <= end) //プラスに向かって行くケース
+		size = (end - start) + 1;
+	else if (end < start) //マイナスに向かって行くケース
+		size = (start - end) + 1;
+	res = malloc(sizeof(int) * size);
+	if (!res)
+		return (0);
+	if (start == end)
+		res[i] = start;
+	else if (start < end) //プラスに向かって行くケース
+	{
+		while (i < size)
+		{
+			res[i] = start;
+			i++;
+			start++;
+		}
+	}
+	else if (end < start) //マイナスに向かって行くケース
+	{
+		while (i < size)
+		{
+			res[i] = start;
+			i++;
+			start--;
+		}
+	}
+	return (res);
 }
 
-int main()
-{
-    ft_range(0, -5);
-    return (0);
-}
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// // ft_range 関数のプロトタイプ宣言
+// int *ft_range(int start, int end);
+
+// int main() {
+//     int start = 0; // 開始値
+//     int end = 0;   // 終了値
+
+//     // ft_range 関数を呼び出して結果を取得
+//     int *result = ft_range(start, end);
+
+//     // 結果を出力
+//     if (result == NULL) {
+//         printf("メモリの割り当てに失敗しました。\n");
+//     } else {
+//         printf("result:[");
+//         // 配列の要素を出力
+//         for (int i = 0; i < (end - start + 1); i++) {
+//             printf("%d", result[i]);
+//             if (i != (end - start)) {
+//                 printf(", ");
+//             }
+//         }
+//         printf("]\n");
+//         // 結果のメモリを解放
+//         free(result);
+//     }
+
+//     return 0;
+// }
