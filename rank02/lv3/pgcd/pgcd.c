0dc1db2831cb78134ce2d4d11e5fd6114831ae91
Miyuki Ito <mito@c3r3p6.hive.fi>
2024-03-29 15:25:51 +0200

    added another solution for pgcd

--- rank02/lv3/pgcd/pgcd.c
+++ rank02/lv3/pgcd/pgcd.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void putnum(int num)
+{
+	char c;
+	if (num < 10)
+	{
+		c = num + '0';
+		write(1, &c, 1);
+	}
+	else
+	{
+		putnum(num / 10);
+		c = num % 10 + '0';
+		write(1, &c, 1);
+	}
+}
 
+void ft_pgcd(char *char_num1, char *char_num2)
+{
+	int num1 = atoi(char_num1);
+	int num2 = atoi(char_num2);
+	int div = 1;
+	int res = 0;
+	while (div < num1 || div < num2)
+	{
+		if ((num1 % div == 0) && (num2 % div == 0))
+			res = div;
+		div++;
+	}
+	putnum(res);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 3)
+		ft_pgcd(argv[1], argv[2]);
+	write(1, "\n", 1);
+}
+
+
+/*
 int main(int argc, char **argv)
 {
 	if (argc == 3)
@@ -21,3 +60,5 @@ int main(int argc, char **argv)
 	printf("\n");
 	return (0);
 }
+
+*/
