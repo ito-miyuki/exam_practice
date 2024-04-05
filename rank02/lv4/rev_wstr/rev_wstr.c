a8daaa13d9971008dbf26b5311b410e8760ca1d3
Miyuki Ito <mito@c1r2p3.hive.fi>
2024-04-05 16:13:51 +0300

    added more solution for rev wstr

--- rank02/lv4/rev_wstr/rev_wstr.c
+++ rank02/lv4/rev_wstr/rev_wstr.c
@@ -2,72 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 
-// void ft_putchar(char c) {
-//     write(1, &c, 1);
-// }
+/*
+文字列の後ろに前に向かっていく
+単語が見つかったら、その単語を逆順にして出力。単語の間にはスペースを挿入。
+*/
 
-// // 文字列の単語の数を数える関数
-// int word_count(char *str) {
-//     int i = 0;
-//     int count = 0;
-//     while (str[i]) 
-//     {
-//         //1こ前の文字が空白だったらカウントする
-//         //一番最初の文字も数えるためにi == 0も条件にいれる
-//         if ((str[i-1] == ' ' || str[i-1] == '\t' || i == 0) 
-//         && (str[i] != ' ' && str[i] != '\t')) 
-//         {
-//             count++;
-//         }
-//         i++;
-//     }
-//     return count;
-// }
-
-// // 単語を分割して、それらの配列を返す関数
-// char **split_words(char *str) 
+// int	is_space(char c)
 // {
-//     int wc = word_count(str);
-//     char **words = malloc(sizeof(char*) * (wc + 1)); // 単語の配列にメモリを割り当てる
-//     int i = 0, j, word_len;
-
-//     for (int w = 0; w < wc; w++) {
-//         while (str[i] == ' ' || str[i] == '\t') i++; // 空白をスキップ
-//         word_len = 0;
-//         j = i;
-//         // 単語の長さを計算
-//         while (str[j] && str[j] != ' ' && str[j] != '\t') {
-//             word_len++;
-//             j++;
-//         }
-//         words[w] = (char*)malloc(sizeof(char) * (word_len + 1)); // 単語にメモリを割り当てる
-//         strncpy(words[w], &str[i], word_len); // 単語をコピー
-//         words[w][word_len] = '\0'; // ヌル終端を追加
-//         i += word_len;
-//     }
-//     words[wc] = NULL; // 配列の終端を示す
-//     return words;
+// 	if ((c == ' ') || (c == '\t'))
+// 		return (1);
+// 	return (0);
 // }
 
-// int main(int argc, char **argv) {
-//     if (argc == 2) {
-//         char **words = split_words(argv[1]);
-//         for (int i = word_count(argv[1]) - 1; i >= 0; i--) {
-//             // 単語を逆順に出力
-//             write(1, words[i], strlen(words[i]));
-//             if (i > 0) {
-//                 ft_putchar(' '); // 単語の間にスペースを挿入
-//             }
-//             free(words[i]); // 割り当てたメモリを解放
-//         }
-//         free(words); // 単語の配列のメモリを解放
-//     }
-//     ft_putchar('\n');
-//     return 0;
+// void	rev_wstr(char *str)
+// {
+// 	int	i;
+// 	int	j;
+// 	int	first_word;
+
+// 	i = 0;
+// 	first_word = 1;
+// 	while (str[i] != '\0')
+// 		i++;
+// 	while (i >= 0)
+// 	{
+// 		while (i >= 0 && (str[i] == '\0' || is_space(str[i])))
+// 			i--;
+// 		j = i;
+// 		while (j >= 0 && !is_space(str[j]))
+// 			j--;
+// 		if (first_word == 0)
+// 			write(1, " ", 1);
+// 		write(1, str[j + 1], i - j);
+// 		first_word = 0;
+// 		i = j;
+// 	}
+// }
+
+// int	main(int argc, char **argv)
+// {
+// 	if (argc == 2)
+// 		rev_wstr(argv[1]);
+// 	write(1, "\n", 1);
+// 	return (0);
 // }
 
 //other solution
 
+//まだ途中
 int	is_space(char c)
 {
 	if ((c == ' ') || (c == '\t'))
@@ -77,26 +59,26 @@ int	is_space(char c)
 
 void	rev_wstr(char *str)
 {
-	int	i;
-	int	j;
-	int	first_word;
+	int	i = 0;
+	int	j = 0;
+	int begin = 0;
+	int	flag = 0;
 
-	i = 0;
-	first_word = 1;
+	while (is_space(str[i]) || str[i] == '\0')
+		i++;
+	begin = i;
 	while (str[i] != '\0')
 		i++;
-	while (i >= 0)
+	while (i >= begin)
 	{
-		while (i >= 0 && (str[i] == '\0' || is_space(str[i])))
+		while (is_space(str[i]) || str[i] == '\0')
 			i--;
 		j = i;
 		while (j >= 0 && !is_space(str[j]))
 			j--;
-		if (first_word == 0)
+		if (flag == 0)
 			write(1, " ", 1);
 		write(1, str[j + 1], i - j);
-		first_word = 0;
-		i = j;
 	}
 }
 
@@ -107,3 +89,39 @@ int	main(int argc, char **argv)
 	write(1, "\n", 1);
 	return (0);
 }
+
+
+// int main (int argc, char **argv)
+// {
+//     int i = 0;
+//     int start = 0;
+//     int end = 0;
+//     int begin = 0;
+
+//     if (argc == 2)
+//     {
+//         while (argv[1][i] == ' ' || argv[1][i] == '\0' || argv[1][i] == '\t')
+//             i++;
+//         begin = i;
+//         while (argv[1][i])
+//                 i++;
+//         while (begin <= i)
+//         {
+//             while (argv[1][i] == ' ' || argv[1][i] == '\0' || argv[1][i] == '\t')
+//                 i--;
+//             end = i;
+//             while (argv[1][i] != ' ' && argv[1][i] != '\0' && argv[1][i] != '\t')
+//                 i--;
+//             start = i + 1;
+//             int flag = start;
+//             while (start <=end)
+//             {
+//                 write (1, &argv[1][start], 1);
+//                 start++;
+//             }
+//             if (flag != begin)
+//                 write (1, " ", 1);
+//         }
+//     }
+//     write (1, "\n", 1);
+// }
