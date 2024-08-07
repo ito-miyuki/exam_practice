af1e479a4665091f4e3e4479295772784a31bb1d
Miyuki Ito <mitou.1221@gmail.com>
2024-08-07 15:56:57 +0300

    completed practice.c not tested in grademe

--- rank04/practice.c
+++ rank04/practice.c
@@ -1,9 +1,56 @@
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+
+int print_error(char *msg, char *argv)
+{
+	while (*msg)
+		write(2, msg++, 1);
+	if (argv)
+		while(*argv)
+			write(2, argv++, 1);
+	write(2, "\n", 1);
+    return (1);
+}
+
+int ft_cd(char **argv, int i)
+{
+    if (i != 2)
+        return (print_error("error: cd: bad arguments", NULL));
+    if (chdir(argv[i]) == -1)
+        return (print_error("error: cd: cannot change directory to ", argv[i]));
+    return (0);
+}
 
 int exec(char **argv, int i, char **envp)
 {
+    int fd[2];
+    int status;
+    int has_pipe = argv[i] && !strcmp(argv[i], "|"); 
+    // --> it i checking if the argv[i] exist and if it is true, has_pipe int fill be 1 (true).
 
+    if (!has_pipe && strcmp(*argv, "cd") == 0) //it means if it is cd
+        return (ft_cd(argv, i));
+
+    if (has_pipe && pipe(fd) == -1)
+        return (print_error("error: fatal", NULL));
+
+    int pid = fork();
+    if (!pid)
+    {
+        argv[i] = 0;
+        if (has_pipe && (dup2(fd[i], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+            return (print_error("error: fatal", NULL));
+        if (strcmp(*argv, "cd") == 0) // it means if it is cd
+            return (ft_cd(argv, i));
+        execve(*argv, argv, envp);
+        return (print_error("error: cannot excute ", *argv));
+    }
+
+    waitpid(pid, &status, 0);
+    if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+        return (print_error("error: fatal", NULL));
+    return (WIFEXITED(status) && WEXITSTATUS(status));
 }
 
 int main(int argc, char **argv, char **envp)
@@ -24,4 +71,5 @@ int main(int argc, char **argv, char **envp)
                 status = exec(argv, i, envp);
         }
     }
+    return status;
 }
